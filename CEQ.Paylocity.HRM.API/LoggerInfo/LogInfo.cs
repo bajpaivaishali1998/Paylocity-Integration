ec2c8d0768abab37643d83739d2660f7f2b221d4
bajpaivaishali1998 <vaishali.bajpai@cloudeq.com>
2024-02-09 14:14:09 +0530

    commit message

---
+++ CEQ.Paylocity.HRM.API/LoggerInfo/LogInfo.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CEQ.Paylocity.HRM.API.LoggerInfo
+{
+    public class LogInfo : TableEntity
+    {
+        public string Method_Name { get; set; }
+        public string Method_Start { get; set; }
+        public string Method_End { get; set; }
+        public string ReqInput { get; set; }
+        public string ApiUrl { get; set; }
+        public string ResOutput { get; set; }
+        public string ErrorException { get; set; }
+        public string LogMessage { get; set; }
+        public string StatusCode { get; set; }
+        public LogInfo()
+        {
+            Method_Name = "NA"; Method_Start = "NA"; ReqInput = "NA"; ApiUrl = "NA"; ResOutput = "NA"; Method_End = "NA"; ErrorException = "NA"; LogMessage = "NA"; StatusCode = "NA";
+        }
+    }
+
+}
---
+++ CEQ.Paylocity.HRM.API/Models/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CEQ.Paylocity.HRM.API.Models
+{
+    public class Employee
+    {
+        public string employeeId { get; set; }
+        public string address1 { get; set; }
+        public string address2 { get; set; }
+        public string autoPayType { get; set; }
+        public int baseRate { get; set; }
+        public string city { get; set; }
+        public string costCenter1 { get; set; }
+        public string costCenter2 { get; set; }
+        public string costCenter3 { get; set; }
+        public int defaultHours { get; set; }
+        public string employeeStatus { get; set; }
+        public string employmentType { get; set; }
+        public string federalFilingStatus { get; set; }
+        public string firstName { get; set; }
+        public string sex { get; set; }
+        public string hireDate { get; set; }
+        public string homePhone { get; set; }
+        public string lastName { get; set; }
+        public string maritalStatus { get; set; }
+        public string personalMobilePhone { get; set; }
+        public string payFrequency { get; set; }
+        public string personalEmailAddress { get; set; }
+        public string payType { get; set; }
+        public string ratePer { get; set; }
+        public int salary { get; set; }
+        public string state { get; set; }
+        public string ssn { get; set; }
+        public string stateFilingStatus { get; set; }
+        public string suiState { get; set; }
+        public string taxForm { get; set; }
+        public string taxState { get; set; }
+        public string userName { get; set; }
+        public string workEmailAddress { get; set; }
+        public string zip { get; set; }
+    }
+
+}
